7b396b349c5883a458d7eb54d42ea79bb59b4161
Roman Zhukov <ro.zhuckov2013@yandex.ru>
2023-03-23 23:40:44 +0300

    change style button

--- src/Components/NewsList/NewsListItem/ItemPage/item-page.tsx
+++ src/Components/NewsList/NewsListItem/ItemPage/item-page.tsx
@@ -9,34 +9,34 @@ import './item-page.scss';
 import './item-page.media.scss';
 
 const ItemPage = () => {
-    const [targetNews,setTargetNews] = useState<IArrNews>();
-    const targetDate = useSelector<IState,Date>(state => state.targetDate);
-    const arrNews = useSelector<IState,IArrNews[]>(state => state.arrNews);
-    const targetID = useSelector<IState,number>(state => state.targetID);
+    const [targetNews, setTargetNews] = useState<IArrNews>();
+    const targetDate = useSelector<IState, Date>(state => state.targetDate);
+    const arrNews = useSelector<IState, IArrNews[]>(state => state.arrNews);
+    const targetID = useSelector<IState, number>(state => state.targetID);
     const dispatch = useDispatch();
     const { pathname } = useLocation();
 
     useEffect(() => {
         arrNews.forEach(item => {
-            if(item?.id === targetID) {
+            if (item?.id === targetID) {
                 setTargetNews(item);
-                if(item.kids !== undefined) {
+                if (item.kids !== undefined) {
                     console.log(item.kids);
                     dispatch(setParentComentsArrID(item.kids));
                 }
             }
         });
         dispatch(setDisabledBtn(true));
-    },[])
+    }, [])
 
     useEffect(() => {
-        window.scrollTo(0,0)
+        window.scrollTo(0, 0)
     }, [pathname])
 
     return (
         <div className='item_page'>
             <div className='item_page__wrapper'>
-                <Link to='/'><span className='item_page__back'>&#8617; back</span></Link>
+                <Link to='/'><span className='item_page__back'>go back</span></Link>
                 <div className='item_page__title'>
                     <h3>{targetNews?.title}</h3>
                     <span>Rating: {targetNews?.score}</span>
@@ -46,7 +46,7 @@ const ItemPage = () => {
                     <span>Date: <ReactTimeAgo date={targetDate} locale="en-US" /></span>
                 </div>
                 <a className='item_page__url' href={targetNews?.url}>Link to the source</a>
-                <ItemComents/>
+                <ItemComents />
             </div>
         </div>
     )
